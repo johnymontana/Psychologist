ccc2eb99fecdb01c07cb30c92d4b243360290878
William Lyon <lyonwj@gmail.com>
2012-02-26 08:37:07 -0700

    Initial Commit

---
+++ Psychologist/PsychologistAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PsychologistAppDelegate.h
+//  Psychologist
+//
+//  Created by William Lyon on 2/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PsychologistAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Psychologist/PsychologistViewController.h
@@ -0,0 +1,13 @@
+//
+//  PsychologistViewController.h
+//  Psychologist
+//
+//  Created by William Lyon on 2/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PsychologistViewController : UIViewController
+
+@end
